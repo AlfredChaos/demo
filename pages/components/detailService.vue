b8d26104ed369f86c8ea40edd9ffcbcfc9ebaf14
AlfredChaos <Caojie_the42@outlook.com>
2024-09-24 00:16:44 +0800

    detail sonComp

---
+++ pages/components/detailService.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="service">
+			<view class="bg"></view>
+			<view class="serviceContent">
+				<view class="serviceContentTitle">
+					服务说明<i class="iconfont" @click="closeBtn">&#xe62b;</i>
+				</view>
+				<view class="serviceList">
+					<view class="serviceItem">
+						<view class="serviceItemTitle">
+							<i class="iconfont">&#xe8af;</i>
+							<text>花呗分期</text>
+						</view>
+						<view class="serviceItemContent">
+							花呗分期是由蚂蚁花呗联合魅族商城共同推出的“先购物后付款”的支付体验，支持预支蚂蚁花呗额度，免息期最长可达41天，可选择分3、6、12期还款。
+						</view>
+					</view>
+					<view class="serviceItem">
+						<view class="serviceItemTitle">
+							<i class="iconfont">&#xe8af;</i>
+							<text>花呗分期</text>
+						</view>
+						<view class="serviceItemContent">
+							花呗分期是由蚂蚁花呗联合魅族商城共同推出的“先购物后付款”的支付体验，支持预支蚂蚁花呗额度，免息期最长可达41天，可选择分3、6、12期还款。
+						</view>
+					</view>
+					<view class="serviceItem">
+						<view class="serviceItemTitle">
+							<i class="iconfont">&#xe8af;</i>
+							<text>花呗分期</text>
+						</view>
+						<view class="serviceItemContent">
+							花呗分期是由蚂蚁花呗联合魅族商城共同推出的“先购物后付款”的支付体验，支持预支蚂蚁花呗额度，免息期最长可达41天，可选择分3、6、12期还款。
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="serviceBotton" @click="closeBtn">
+				确定
+			</view>
+		</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			closeBtn() {
+				// 子组件向父组件传值
+				this.$emit('close');
+			}
+		}
+	}
+</script>
+
+<style>
+	.service{
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+	}
+	.bg{
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		background: rgba(0, 0, 0, 0.5);
+	}
+	.serviceContent{
+		position: absolute;
+		height: 1070rpx;
+		background: white;
+		bottom: 0;
+		width: 100%;
+}
+	.serviceContentTitle{
+		height: 110rpx;
+		text-align: center;
+		font-size: 32rpx;
+		line-height: 110rpx;
+		position: relative;
+	}
+	.serviceContentTitle i{
+		position: absolute;
+		top: 40rpx;
+		right: 30rpx;
+		line-height: 40rpx;
+	}
+	.serviceItem{
+		border-bottom: 1rpx solid #e7e7e7;
+		padding-bottom: 34rpx;
+	}
+	.serviceItemTitle{
+		line-height: 60rpx;
+	}
+	.serviceItemTitle i{
+		margin-left: 20rpx;
+		margin-right: 10rpx;
+		color: #95e1df;
+		font-size: 24rpx;
+	}
+	.serviceItemTitle text{
+		font-size: 28rpx;
+	}
+	.serviceItemContent{
+		font-size: 24rpx;
+		color: #999999;
+		line-height: 34rpx;
+		padding: 0 50rpx;
+	}
+	.serviceBotton{
+		position: absolute;
+		bottom: 0;
+		height: 90rpx;
+		background: #00c3f5;
+		font-size: 28rpx;
+		color: #ffffff;
+		width: 100%;
+		text-align: center;
+		line-height: 90rpx;
+	}
+</style>
--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -35,7 +35,7 @@
 				<view class="shopPrice"><text>￥</text>799.00<label>￥599.00</label></view>
 				<view class="shopSale">月销量：5000件</view>
 			</view>
-			<view class="shopAssure">
+			<view class="shopAssure" @click="showService">
 				<view class="shopAssureItem">
 					<i class="iconfont">&#xe60d;</i>全场包邮
 				</view>
@@ -112,19 +112,111 @@
 				</view>
 			</view>
 		</view>
+		<!-- <view style="height: 100rpx; width: 100%;"></view> -->
+		<view class="detailBottom">
+			<view class="bottomItem">
+				<view class="bottomItemIndex">
+					<image src="../../static/tabbar1.png" mode=""></image>
+					<text>首页</text>
+				</view>
+				<view class="bottomItemIndex">
+					<image src="../../static/tabbar3.png" mode=""></image>
+					<text>购物车</text>
+				</view>
+			</view>
+			<view class="bottomButton cartButton">
+				加入购物车
+			</view>
+			<view class="bottomButton buyButton">
+				立即购买
+			</view>
+		</view>
+		
+		<detailService @close="closeService" v-if="serviceFlag"></detailService>
+		
+		<view class="attrDialog">
+			<view class="bg"></view>
+			<view class="attrDialogContent">
+				<view class="contentTop">
+					<image src="../../static/commodity/p1.png" mode=""></image>
+					<view class="contentTopText">
+						<text class="price">￥299.00</text>
+						<text class="attr">魅族16S PRO，黑色</text>
+					</view>
+					<i class="iconfont">&#xe62b;</i>
+				</view>
+				<view class="attrList">
+					<view class="attrItem">
+						<view class="attrItemTitle">
+							版本
+						</view>
+						<view class="attrItemValue">
+							<view class="attrValueItem activeAttr">
+								魅族16S Pro
+							</view>
+							<view class="attrValueItem">
+								魅族16S Plus
+							</view>
+						</view>
+					</view>
+					<view class="attrItem">
+						<view class="attrItemTitle">
+							颜色
+						</view>
+						<view class="attrItemValue">
+							<view class="attrValueItem activeAttr">
+								黑色
+							</view>
+							<view class="attrValueItem">
+								蓝色
+							</view>
+						</view>
+					</view>
+					<view class="attrItem">
+						<view class="attrItemTitle">
+							数量
+						</view>
+						<view class="attrItemValue">
+							<view class="shopNumber">
+								<view class="button">-</view>
+								<view><input type="text" value="1" /></view>
+								<view class="button">+</view>
+							</view>
+							<text class="stock">（库存：5件）</text>
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="attrButton">
+				立即购买
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import detailService from '../components/detailService.vue'
 	export default {
 		data() {
 			return {
-				tab: 0
+				tab: 0,
+				serviceFlag: false
 			}
+		},
+		components: {
+			detailService
 		},
 		methods: {
 			tabChange(index){
 				this.tab = index
+			},
+			// 显示服务弹窗
+			showService(){
+				this.serviceFlag = true;
+			},
+			// 关闭服务弹窗
+			closeService() {
+				this.serviceFlag = false;
 			}
 		}
 	}
@@ -270,5 +362,174 @@
 		text-align: center;
 		font-size: 24rpx;
 		color: #000000;
+	}
+	.detailBottom{
+		height: 100rpx;
+		width: 100%;
+		background: #ffffff;
+		position: fixed;
+		bottom: 0;
+		display: flex;
+	}
+	.bottomButton{
+		width: 275rpx;
+		color: #ffffff;
+		font-size: 28rpx;
+	}
+	.bottomItem{
+		flex: 1;
+		display: flex;
+	}
+	.bottomItemIndex{
+		width: 50%;
+	}
+	.bottomItemIndex image{
+		width: 40rpx;
+		height: 40rpx;
+		display: block;
+		margin: 0 auto;
+		margin: 10rpx auto 0;
+	}
+	.bottomItemIndex text {
+		font-size: 24rpx;
+		color: #999999;
+		text-align: center;
+		display: block;
+		height: 40rpx;
+		line-height: 40rpx;
+	}
+	.cartButton{
+		background: #0ebcef;
+		text-align: center;
+		line-height: 100rpx;
+	}
+	.buyButton{
+		background: #017cfe;
+		text-align: center;
+		line-height: 100rpx;
+	}
+	
+	.attrDialog{
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+	}
+	.attrDialog .bg{
+		background: rgb(0, 0, 0, 0.5);
+		height: 100%;
+		width: 100%;
+	}
+	.attrDialogContent{
+		height: 1070rpx;
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		background: white;
+	}
+	.contentTop{
+		height: 200rpx;
+		position: relative;
+		border-bottom: 1rpx solid #e5e5e5;
+	}
+	.contentTop i{
+		position: absolute;
+		top: 25rpx;
+		right: 30rpx;
+	}
+	.contentTop image{
+		position: absolute;
+		top: -40rpx;
+		left: 30rpx;
+		width: 210rpx;
+		height: 210rpx;
+		background: white;
+		border: 1rpx solid #e5e5e5;
+	}
+	.contentTopText{
+		margin-left: 280rpx;
+	}
+	.contentTopText .price{
+		height: 44rpx;
+		color: #f0415f;
+		font-size: 36rpx;
+		display: block;
+		line-height: 44rpx;
+		padding-top: 24rpx;
+	}
+	.contentTopText .attr{
+		font-size: 24rpx;
+		color: #000000;
+		line-height: 40rpx;
+		height: 40rpx;
+	}
+	.attrList{
+		padding: 0 30rpx;
+	}
+	.attrItemTitle {
+		color: #999999;
+		line-height: 70rpx;
+		font-size: 28rpx;
+	}
+	.attrItem{
+		height: auto;
+		overflow: hidden;
+	}
+	.attrValueItem{
+		padding: 0 30rpx;
+		border: 1rpx solid #e5e5e5;
+		height: 70rpx;
+		line-height: 70rpx;
+		float: left;
+		font-size: 24rpx;
+		color: #666666;
+		margin-right: 30rpx;
+	}
+	.attrValueItem.activeAttr{
+		color: #00c3f5;
+		border-color: #00c3f5;
+	}
+	
+	.shopNumber{
+		width: 240rpx;
+		height: 70rpx;
+		line-height: 70rpx;
+		border: 1rpx solid #e5e5e5;
+		display: flex;
+		float: left;
+	}
+	.shopNumber .button{
+		width: 70rpx;
+		line-height: 70rpx;
+		text-align: center;
+	}
+	.shopNumber input{
+		flex: 1;
+		border-left: 1rpx solid #e5e5e5;
+		border-right: 1rpx solid #e5e5e5;
+		width: 100rpx;
+		height: 70rpx;
+		line-height: 70rpx;
+		font-size: 24rpx;
+		text-align: center;
+	}
+	.stock {
+		height: 70rpx;
+		line-height: 70rpx;
+		font-size: 24rpx;
+		color: #999999;
+		margin-left: 20rpx;
+	}
+	.attrButton{
+		height: 90rpx;
+		line-height: 90rpx;
+		position: absolute;
+		bottom: 0;
+		background: #0ebcef;
+		font-size: 28rpx;
+		width: 100%;
+		text-align: center;
+		color: white;
 	}
 </style>
